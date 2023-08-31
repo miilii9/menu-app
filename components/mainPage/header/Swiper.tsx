9a22c6605eb2be0379ba76f0574275a97463d2b4
miilii9 <milad.ek99@yahoo.com>
2023-08-31 03:59:36 +0330

    fix swiper full

--- components/mainPage/header/Swiper.tsx
+++ components/mainPage/header/Swiper.tsx
@@ -55,22 +55,30 @@ export const SwiperComp = ({
         </span>
       </div>
       <Swiper
-        slideToClickedSlide={true}
-        centeredSlides={true}
+        onActiveIndexChange={(swiper) => setSwiperIndex(swiper.activeIndex)}
         className=' w-full h-full p-4'
         slidesPerView={6}
         spaceBetween={50}
         modules={[Navigation]}
-        onActiveIndexChange={(swiper) => setSwiperIndex(swiper.activeIndex)}>
+        centeredSlides={true}
+        onSlideChange={() => {
+          // const activeId = document.querySelector(".swiper-slide-active")?.id;
+          // // console.log(activeId);
+          // let topOfElement =
+          //   (document.getElementById(`${activeId}`)?.offsetTop as number) - 200;
+          // window.scroll({ top: topOfElement, behavior: "smooth" });
+          // scrollToSection(`${activeId}`);
+        }}
+        slideToClickedSlide={true}>
         {data.categorys.map((item: any, q: any) => (
           <SwiperSlide
             className='w-full my-3 '
             key={q}
             id={item.name.replace(/\s+/g, "")}
             onClick={() => {
-              scrollToSection(item.name.replace(/\s+/g, ""));
+              scrollToSection(item.name);
             }}>
-            <div className=' bg-[#ffffff80]  w-32 flex flex-col justify-center items-center align-middle  rounded-2xl text-center gap-2 p-8 h-32'>
+            <div className=' bg-[#ffffff80]   flex flex-col justify-center items-center align-middle  rounded-2xl text-center gap-2 p-14 py-16 h-32'>
               <img
                 src='pics/cat.png'
                 alt='pic'
@@ -86,79 +94,3 @@ export const SwiperComp = ({
     </div>
   );
 };
-// sdddddddddddddddddddddddddddddd
-
-//   const [swiperIndex, setSwiperIndex] = useState<any>(0);
-//   const swiperToId = () => {
-//     const swiperSlides = document.getElementsByClassName("swiper-slide");
-//     const activeId = swiperSlides[swiperIndex].id;
-//     let topOfElement =
-//       (document.getElementById(activeId)?.getBoundingClientRect().x as any) -
-//       200;
-//     window.scroll({ top: topOfElement, behavior: "smooth" });
-//   };
-//   useEffect(() => {
-//     swiperToId();
-//     return () => {};
-//   }, [swiperIndex]);
-
-//   return (
-//     <div
-//       className={`  w-full  flex flex-col items-center align-middle justify-start`}>
-//       {order.length ? (
-//         <span
-//           className='absolute top-2 left-2 z-[999] orderIcon'
-//           onClick={toggleDrawer("left", true)}>
-//           <div className='relative'>
-//             <span className='absolute -top-1 -right-3 bg-orange-600 text-sm rounded-full w-5 h-5 text-center align-middle  text-white'>
-//               {order.length}
-//             </span>
-//             <FaBookOpen className={"text-4xl"} />
-//           </div>{" "}
-//         </span>
-//       ) : null}
-//       <div className=' mb-2 '>
-//         <span
-//           className={` ${style.allCategories} text-xs font-medium bg-[rgba(255, 255, 255, 0.15)] px-4 py-2 w-full text-center rounded-3xl cursor-pointer`}>
-//           همه دسته بندی ها در یک نگاه
-//         </span>
-//       </div>
-//       <Swiper
-//         className=' w-full h-full p-4'
-//         slidesPerView={6}
-//         spaceBetween={50}
-//         modules={[Navigation]}
-//         centeredSlides={true}
-//         onSlideChange={() => {
-//           // const activeId = document.querySelector(".swiper-slide-active")?.id;
-//           // // console.log(activeId);
-//           // let topOfElement =
-//           //   (document.getElementById(`${activeId}`)?.offsetTop as number) - 200;
-//           // window.scroll({ top: topOfElement, behavior: "smooth" });
-//           // scrollToSection(`${activeId}`);
-//         }}
-//         slideToClickedSlide={true}>
-//         {data.categorys.map((item: any, q: any) => (
-//           <SwiperSlide
-//             className='w-full my-3 '
-//             key={q}
-//             id={item.name}
-//             onClick={() => {
-//               scrollToSection(item.name);
-//             }}>
-//             <div className=' bg-[#ffffff80]   flex flex-col justify-center items-center align-middle  rounded-2xl text-center gap-2 p-14 py-16 h-32'>
-//               <img
-//                 src='pics/cat.png'
-//                 alt='pic'
-//                 className='self-center w-full aspect-square'
-//               />
-//               <span className='w-32 text-center font-bold opacity-100'>
-//                 {item.name}
-//               </span>
-//             </div>
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//     </div>
-//   );
-// };
